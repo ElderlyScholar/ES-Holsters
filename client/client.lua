b41b6ac4792eac5fa004279c72e6cb049cdc6a31
ElderlyScholar <pinewoodstaterp@gmail.com>
2024-05-18 03:53:12 +1000

    Add files via upload

--- client/client.lua
+++ client/client.lua
@@ -1,25 +1,15 @@
 local QBCore = exports["qb-core"]:GetCoreObject()
 
-local function HolsterDraw()
-    local Ped = PlayerPedId()
-    local Holster = GetPedDrawableVariation(Ped, Config.DrawableType)
-
-    for k, v in pairs(Config.Drawables) do
-        if Holster == k then
-            SetPedComponentVariation(Ped, Config.DrawableType, v, 1, 0)
-        end
-    end
-end
-
-exports("HolsterDraw", HolsterDraw)
-
 RegisterNetEvent("ES-Holsters:client:Draw", function()
     local Ped = PlayerPedId()
     local Holster = GetPedDrawableVariation(Ped, Config.DrawableType)
 
+
     for k, v in pairs(Config.Drawables) do
-        if Holster == k then
+        if tostring(Holster) == tostring(k) then
             SetPedComponentVariation(Ped, Config.DrawableType, v, 1, 0)
+        elseif tostring(Holster) == tostring(v) then
+            SetPedComponentVariation(Ped, Config.DrawableType, tonumber(k), 1, 0)
         end
     end
 end)
